974b4548727ec3139d341571d9446db6fcf2a682
aciusjob1 <aciusjob1@gmail.com>
2026-05-27 12:34:40 +0300

    fix: simplify accept_terms_view to use has_accepted_terms field directly

--- apps/identity/views_terms.py
+++ apps/identity/views_terms.py
@@ -1,27 +1,24 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from apps.compliance.engine import must_accept_policy, accept_policy, get_active_policy
 from apps.identity.utils.network import get_client_ip
 
 def accept_terms_view(request):
     if not request.user.is_authenticated:
         return redirect('login')
-    
+
     # Already accepted
-    if not must_accept_policy(request.user, 'terms_of_service'):
+    if request.user.has_accepted_terms:
         return redirect('member-dashboard')
-    
-    policy = get_active_policy('terms_of_service')
+
     error = None
-    
+
     if request.method == 'POST':
         if request.POST.get('agree') != 'on':
             error = 'You must accept the Terms of Service to continue.'
         else:
-            ip = get_client_ip(request)
-            ua = request.META.get('HTTP_USER_AGENT', '')
-            accept_policy(request.user, 'terms_of_service', ip, ua)
+            request.user.has_accepted_terms = True
+            request.user.save(update_fields=['has_accepted_terms'])
             messages.success(request, "Welcome! You have accepted the Terms of Service.")
             return redirect('member-dashboard')
-    
-    return render(request, 'accept_terms.html', {'policy': policy, 'error': error})
+
+    return render(request, 'accept_terms.html', {'error': error})

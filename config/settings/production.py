7a8489f9796e64917069429d9a3d7fd44b596176
aciusjob1 <aciusjob1@gmail.com>
2026-05-27 07:29:13 +0300

    fix: remove extra parentheses in cloudinary config

--- config/settings/production.py
+++ config/settings/production.py
@@ -27,8 +27,8 @@ MEDIA_URL = '/media/'
 
 # Cloudinary media storage
 CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME', '')),
-    'API_KEY': os.environ.get('CLOUDINARY_API_KEY', '')),
-    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET', '')),
+    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME', ''),
+    'API_KEY': os.environ.get('CLOUDINARY_API_KEY', ''),
+    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET', ''),
 }
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'

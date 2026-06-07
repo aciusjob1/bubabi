f40b0e0866041240fb7c081adfdb14e5ae14a29b
aciusjob1 <aciusjob1@gmail.com>
2026-06-07 08:47:13 +0300

    security: add HSTS and security headers

--- config/settings/production.py
+++ config/settings/production.py
@@ -4,6 +4,19 @@ from .base import *
 
 DEBUG = False
 
+# Security headers
+SECURE_HSTS_SECONDS = 31536000  # 1 year
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_BROWSER_XSS_FILTER = True
+X_FRAME_OPTIONS = 'DENY'
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = True
+
+
+
 ALLOWED_HOSTS = [
     'bubabi.onrender.com',
     'bubabi.fly.dev',
